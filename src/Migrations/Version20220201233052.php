7b8f126417c867b3caaba56dd3752187c7062663
Jakub Staňo <ajax770sk@gmail.com>
2022-02-01 20:37:26 +0100

    Add migrations

--- src/Migrations/Version20220201233052.php
+++ src/Migrations/Version20220201233052.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220130233052 extends AbstractMigration
+final class Version20220201233052 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,9 +23,10 @@ final class Version20220130233052 extends AbstractMigration
         $this->addSql('ALTER TABLE sylius_invoicing_plugin_tax_item DROP CONSTRAINT FK_2951C61C2989F1FD');
         $this->addSql('ALTER TABLE sylius_invoicing_plugin_line_item DROP CONSTRAINT FK_C91408292989F1FD');
         $this->addSql('ALTER TABLE sylius_invoicing_plugin_invoice CHANGE id id INT AUTO_INCREMENT NOT NULL');
+        //
         $this->addSql('ALTER TABLE sylius_invoicing_plugin_line_item CHANGE invoice_id invoice_id INT NOT NULL');
-        $this->addSql('ALTER TABLE sylius_invoicing_plugin_shop_billing_data ADD company_number VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE sylius_invoicing_plugin_tax_item CHANGE invoice_id invoice_id INT NOT NULL');
+        //
         $this->addSql('ALTER TABLE sylius_invoicing_plugin_tax_item ADD CONSTRAINT FK_2951C61C2989F1FD FOREIGN KEY (invoice_id) REFERENCES sylius_invoicing_plugin_invoice (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE sylius_invoicing_plugin_line_item ADD CONSTRAINT FK_C91408292989F1FD FOREIGN KEY (invoice_id) REFERENCES sylius_invoicing_plugin_invoice (id) ON DELETE CASCADE');
     }
@@ -33,5 +34,8 @@ final class Version20220130233052 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sylius_invoicing_plugin_invoice CHANGE id id VARCHAR(255) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`');
+        $this->addSql('ALTER TABLE sylius_invoicing_plugin_line_item CHANGE invoice_id invoice_id VARCHAR(255) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`');
+        $this->addSql('ALTER TABLE sylius_invoicing_plugin_tax_item CHANGE invoice_id invoice_id VARCHAR(255) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`');
     }
 }
